c2193b10a3a0341ea99e3a60a1acaf9ee7e156b1
StevenDinicQA <stevandinic@gmail.com>
2021-06-18 22:03:50 +0200

    Notification page update and profile page creation

--- src/pages/NotificationSysPage.java
+++ src/pages/NotificationSysPage.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class NotificationSysPage extends BasicPage {
 
@@ -13,8 +15,19 @@ public class NotificationSysPage extends BasicPage {
 
 //	popup message when logged in or entering wrong credentials
 	public WebElement getalertMsg() {
-		return driver.findElement(By.xpath(
-				"//*[contains(@class, 'alert--success') or contains(@class, 'alert--danger')][contains(@style,'display: block')]"));
+		return driver.findElement(By.xpath("//*[contains(@class, 'alert--success')]"));
 	}
 
+//	getting visible text message
+	public String getPopupMsg () {
+		return driver.findElement(By.xpath("//div[@class='div_msg']/ul/li")).getText();
+	}
+	
+//	waiter for the message to disappear
+	public void disappearMsg () {
+		WebDriverWait waiter = new WebDriverWait(driver, 20);
+		WebElement msg = driver.findElement(By.xpath("//div[@class=\"system_message alert]"));
+		
+		waiter.until(ExpectedConditions.attributeToBe(msg, "display:", "none"));
+	}
 }
--- src/pages/ProfilePage.java
+++ src/pages/ProfilePage.java
@@ -1,12 +1,15 @@
 package pages;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 
 public class ProfilePage extends BasicPage{
 
-	public ProfilePage(WebDriver driver) {
-		super(driver);
-		// TODO Auto-generated constructor stub
+	public ProfilePage(WebDriver driver, JavascriptExecutor js) {
+		super(driver, js);
 	}
-
+	
+	
+	
+	
 }
