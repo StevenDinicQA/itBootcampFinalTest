eee718b17aea5633bc069d7b2b5e61fdfe74e72b
StevenDinicQA <stevandinic@gmail.com>
2021-06-18 20:31:43 +0200

    Basic page and Location Popup page update

--- src/pages/LocationPopupPage.java
+++ src/pages/LocationPopupPage.java
@@ -1,44 +1,60 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class LocationPopupPage extends BasicPage{
+public class LocationPopupPage extends BasicPage {
 
-	public LocationPopupPage(WebDriver driver) {
-		super(driver);
+	public LocationPopupPage(WebDriver driver, JavascriptExecutor js) {
+		super(driver, js);
 	}
 
-	public WebElement getSelectLocation () {
+//	location selection element
+	public WebElement getSelectLocation() {
 		return driver.findElement(By.className("location-selector"));
 	}
-	
-	public WebElement getCloseBtn () {
+
+//	close button element
+	public WebElement getCloseBtn() {
 		return driver.findElement(By.xpath("//div[@class=\"model-box-mid location-search\"]/a"));
 	}
-		
-	public WebElement getKeyword () {
+
+//	keywords element
+	public WebElement getKeyword() {
 		return driver.findElement(By.xpath("//*[@id='locality_keyword']"));
 	}
-	
-	public WebElement getLocationItem (String locationName) {
+
+//	location item element
+	public WebElement getLocationItem(String locationName) {
 		return driver.findElement(By.xpath("//li/a[contains(text(), '\" + locationName + \"')]/.."));
 	}
-	
-	public WebElement getLocationInput () {
+
+//	location input element
+	public WebElement getLocationInput() {
 		return driver.findElement(By.id("locality_keyword"));
 	}
-	
-	public WebElement getSubmit () {
+
+//	submit btn element
+	public WebElement getSubmit() {
 		return driver.findElement(By.name("btn_submit"));
 	}
-	
-	public void clickLocation () {
+
+//	click method for location
+	public void clickLocation() {
 		getSelectLocation().click();
 	}
-	
-	
-	
-	
+
+//	location input method
+	public void locationName(String locationName) {
+		getKeyword().click();
+		String loc = getLocationItem(locationName).getAttribute(locationName);
+		js.executeScript("arguments[0].value=arguments[1]", this.getLocationInput(), loc);
+	}
+
+	// close button click
+	public void popupClose() {
+		this.getCloseBtn().click();
+	}
 }
