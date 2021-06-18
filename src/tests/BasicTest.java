71e27e27e74ba67db1d0b3f603bed1d7efa1b9bc
StevenDinicQA <stevandinic@gmail.com>
2021-06-18 21:17:50 +0200

    Basic Test creation

--- src/tests/BasicTest.java
+++ src/tests/BasicTest.java
@@ -1,5 +1,30 @@
 package tests;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
 public abstract class BasicTest {
 
+	protected WebDriver driver;
+	
+	@BeforeMethod
+	public void setup() {
+		System.setProperty("webdriver.chrome.driver", "driver-lib\\chromedriver.exe");
+
+		this.driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
+	}
+	
+	@AfterMethod
+	public void cleanup() throws InterruptedException {
+		Thread.sleep(4000);
+		driver.quit();
+	}
+	
 }
