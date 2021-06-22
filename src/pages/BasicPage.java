4782cff8cade72d248c027dcae76ee22a665275a
StevenDinicQA <stevandinic@gmail.com>
2021-06-22 18:11:43 +0200

    Updated basic page with comments

--- src/pages/BasicPage.java
+++ src/pages/BasicPage.java
@@ -6,14 +6,17 @@ import org.openqa.selenium.WebDriver;
 
 public abstract class BasicPage {
 
+//	attributes for the webdriver and javaScript executor
 	protected WebDriver driver;
 	protected JavascriptExecutor js;
 	
+//	constructor
 	public BasicPage (WebDriver driver, JavascriptExecutor js) {
 		this.driver = driver;
 		this.js = js;
 	}
 	
+//	try/catch method
 	public boolean elementExists (WebDriver driver, By by) {
 		boolean exists = true;
 		try {
