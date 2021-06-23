ce85f32555f0d5a4aa35d7e28d806cac612abd44
StevenDinicQA <stevandinic@gmail.com>
2021-06-23 02:41:44 +0200

    Cart summary page getters and methods added

--- src/pages/CartSummaryPage.java
+++ src/pages/CartSummaryPage.java
@@ -1,12 +1,24 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class CartSummaryPage extends BasicPage{
 
-	public CartSummaryPage(WebDriver driver) {
-		super(driver);
-		// TODO Auto-generated constructor stub
+	public CartSummaryPage(WebDriver driver, JavascriptExecutor js) {
+		super(driver, js);
 	}
-
+	
+//	get clear all button
+	public WebElement getClearBtn () {
+		return driver.findElement(By.xpath("//*[@onclick=\"clearCartItems()\"]"));
+	}
+	
+//	method for clicking the button
+	public void clickClearBtn () {
+		getClearBtn().click();
+	}
+	
 }
