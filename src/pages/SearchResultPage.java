54ed406a571cd61d43b080be12f13962ca0890ca
StevenDinicQA <stevandinic@gmail.com>
2021-06-23 20:18:41 +0200

    Search page update (another update soon)

--- src/pages/SearchResultPage.java
+++ src/pages/SearchResultPage.java
@@ -1,12 +1,29 @@
 package pages;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class SearchResultPage extends BasicPage{
 
-	public SearchResultPage(WebDriver driver) {
-		super(driver);
-		// TODO Auto-generated constructor stub
+	public SearchResultPage(WebDriver driver, JavascriptExecutor js) {
+		super(driver, js);
 	}
-
+	
+	public WebElement productName () {
+		return driver.findElement(By.xpath("//*[@class='product-name']/a"));
+	}
+	
+	public List<WebElement> searchNames (){
+		
+		return driver.findElements(By.className("product-name"));
+	}
+	
+	public List<WebElement> resultNum (){
+		return (List<WebElement>) driver.findElement(By.className("product-name"));
+	}
+	
 }
