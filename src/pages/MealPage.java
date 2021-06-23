c494688e1f874c7657cd727b4e784313ba74d8c4
StevenDinicQA <stevandinic@gmail.com>
2021-06-23 02:32:23 +0200

    Meal page with getters and methods

--- src/pages/MealPage.java
+++ src/pages/MealPage.java
@@ -1,12 +1,40 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class MealPage extends BasicPage{
 
-	public MealPage(WebDriver driver) {
-		super(driver);
-		// TODO Auto-generated constructor stub
+	public MealPage(WebDriver driver, JavascriptExecutor js) {
+		super(driver, js);
 	}
-
+	
+//	get item quantity
+	public WebElement getQuantity () {
+		return driver.findElement(By.name("product_qty"));
+	}
+	
+//	get add to cart button
+	public WebElement getAddCartBtn () {
+		return driver.findElement(By.xpath("//*[@class= 'js-proceedtoAddInCart']"));
+	}
+	
+//	get favorite button
+	public WebElement getFavBtn () {
+		return driver.findElement(By.className("itemfav"));
+	}
+	
+//	method for adding to cart
+	public void addToCart (String num) {
+		getQuantity().clear();
+		getQuantity().getAttribute(num);
+		getAddCartBtn().click();
+	}
+	
+//	favorite button click method
+	public void clickFav () {
+		getFavBtn().click();
+	}
 }
