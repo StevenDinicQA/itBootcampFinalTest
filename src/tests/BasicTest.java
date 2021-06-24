139de142a560ac1bcbff45000aa742daf00e7a2f
StevenDinicQA <stevandinic@gmail.com>
2021-06-24 22:22:17 +0200

    Screenshot imports edit

--- src/tests/BasicTest.java
+++ src/tests/BasicTest.java
@@ -1,7 +1,11 @@
 package tests;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
+//import org.apache.commons.io.FileUtils;
+//import org.openqa.selenium.OutputType;
+//import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
@@ -10,9 +14,12 @@ import org.testng.annotations.BeforeMethod;
 public abstract class BasicTest {
 
 	protected WebDriver driver;
+	protected String baseUrl = "http://demo.yo-meals.com/";
+	protected String username = "customer@dummyid.com";
+	protected String password = "12345678a";
 	
 	@BeforeMethod
-	public void setup() {
+	public void setup() throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "driver-lib\\chromedriver.exe");
 
 		this.driver = new ChromeDriver();
@@ -23,6 +30,12 @@ public abstract class BasicTest {
 	
 	@AfterMethod
 	public void cleanup() throws InterruptedException {
+//		
+//		TakesScreenshot scrShot =((TakesScreenshot)driver);
+//		File SrcFile=scrShot.getScreenshotAs(OutputType<X>.FILE);
+//		File DestFile=new File("D:\\QA\\Projects\\Project1\\itBootcampFinalTest\\screenshots");
+		
+		
 		Thread.sleep(4000);
 		driver.quit();
 	}
