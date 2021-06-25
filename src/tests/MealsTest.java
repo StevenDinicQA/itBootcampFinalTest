68b182a170fb272621cd700206aea77e59d39b47
StevenDinicQA <stevandinic@gmail.com>
2021-06-25 02:10:20 +0200

    New test added to the class

--- src/tests/MealsTest.java
+++ src/tests/MealsTest.java
@@ -36,4 +36,39 @@ public class MealsTest extends BasicTest{
 		sa.assertEquals(notSysPage.getalertMsg(), "Meal Added To Cart");
 		notSysPage.disappearMsg();
 	}
+	
+	@Test
+	public void addMealToFavorite () throws InterruptedException{
+		
+//		getting the meal
+		driver.get(baseUrl+ "lobster-shrimp-chicken-quesadilla-combo");
+		Thread.sleep(1000);
+		
+//		closes the popup button
+		locPopPage.getCloseBtn();
+		Thread.sleep(1000);
+		
+//		click on favorite button
+		mealPage.clickFav();
+		
+//		testing if the correct message appears
+		SoftAssert sa = new SoftAssert();
+		sa.assertEquals(notSysPage.getalertMsg(), "Please login first!");
+		notSysPage.disappearMsg();
+		
+//		logging in with credentials from basic test
+		loginPage.loginBtn();
+		loginPage.submitCred(username, password);
+		
+//		getting the meal again
+		driver.get(baseUrl+ "lobster-shrimp-chicken-quesadilla-combo");
+		Thread.sleep(1000);
+		
+//		click on favorite button again
+		mealPage.clickFav();
+		
+//		testing if the correct message appears
+		sa.assertEquals(notSysPage.getalertMsg(), "Product has been added to your favorites!");
+		notSysPage.disappearMsg();
+	}	
 }
